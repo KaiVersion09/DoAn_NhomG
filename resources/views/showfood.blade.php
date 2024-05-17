e1f87e0c46a6743993e531d19027f0186cbded4c
KaiVersion09 <kaizensu123@gmail.com>
2024-05-18 03:20:19 +0700

    commit hien thi food

--- resources/views/showfood.blade.php
+++ resources/views/showfood.blade.php
@@ -1,20 +1,34 @@
 @extends('dashboard')
 @section('content')
-<div class="p-3 mt-2">
+
+
+
+<div class="p-4 mt-4">
+    <div class="row align-items-center bg-light mb-5 p-4">
+        <div class="col-md-6 text-md-start mb-4 mb-md-0">
+            <h1 class="display-2 p-2">WELCOME MY RESTAURANT</h1>
+            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eos abtempora ullam atque voluptatum, alias numquam ut,</p>
+        </div>
+        <div class="col-md-6 text-center p-4">
+            <img src="restaurant-image.jpg" class="img-fluid" alt="Restaurant Image">
+        </div>
+    </div>
+    <h6 class="mb-4 text-center bg-dark p-2 text-white">Các món ăn</h6>
     <div class="row">
         @foreach ($foods as $food)
-        <div class="col-md-2 mb-2">
-            <div class="card bg-dark text-white h-100">
-                <img src="{{ asset($food->food_avatar) }}" class="card-img-top" alt="{{ $food->food_name }}" style="height: 100px; object-fit: cover;">
-                <div class="card-body d-flex flex-column">
+        <div class="col-md-3 mb-2">
+            <div class="card h-100 bg-dark text-white">
+                <img src="{{ asset($food->food_avatar) }}" class="card-img-top" alt="{{ $food->food_name }}">
+                <div class="card-body">
                     <h5 class="card-title">{{ $food->food_name }}</h5>
                     <p class="card-text">{{ $food->food_description }}</p>
-                    <div class="mt-auto">
-                        <p class="card-text text-white">Giá: ${{ $food->food_price }}</p>
-                        <div class="d-flex justify-content-between mt-2">
-                            <a href="#" class="btn btn-primary btn-sm mr-2">Thêm vào giỏ hàng</a>
-                            <a href="#" class="btn btn-secondary btn-sm">Chi tiết</a>
+                    <p class="card-text text-white text-center">Giá: {{ $food->food_price }}</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="rating">
+                            <span class="text-warning">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
                         </div>
+                        <a href="#" class="btn btn-dark btn-sm">Thêm Vào Giỏ</a>
+                        <a href="#" class="btn btn-secondary btn-sm">Chi tiết</a>
                     </div>
                 </div>
             </div>
