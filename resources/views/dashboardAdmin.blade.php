f006d996e03d402514704a63c615772d00369e1d
Nguyen Cao Thanh Tai <tailoveiu@gmail.com>
2024-05-18 13:31:56 +0700

    .

--- resources/views/dashboardAdmin.blade.php
+++ resources/views/dashboardAdmin.blade.php
@@ -18,6 +18,7 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <h1></h1>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
